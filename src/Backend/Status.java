449251fd0f492f6e2f6508359700fc0d10ff8910
Your Name <you@example.com>
2024-11-04 13:27:06 +0500

    Added Task and Status of Task

--- src/Backend/Status.java
+++ src/Backend/Status.java
@@ -1,4 +1,31 @@
 package src.Backend;
 
+import java.util.StringJoiner;
+
 public enum Status {
+    CREATED,
+    IN_PROGRESS,
+    DONE,
+    CANCELED,
+    DEFAULT;
+
+
+    public static String getAllValuesAsString(){
+        Status[] values = values();
+        StringJoiner joiner = new StringJoiner("\n");
+        for (Status value : values) {
+            joiner.add(value.ordinal()+1+"."+value);
+        }
+        return joiner.toString();
+    }
+    public static Status getValueByOrder(int order){
+        Status[] values = values();
+        for (Status value : values) {
+            if (value.ordinal()==order-1){
+                return value;
+            }
+        }
+        return DEFAULT;
+    }
+
 }
